deb3ddc3a7024b0a6cde292724d4b6e700c58655
evermine18 <evermine18@gmail.com>
2024-01-08 15:13:32 +0100

    Testing button with actions

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,52 @@
-mod counter;
 use iced::executor;
 use iced::{Application, Command, Element, Settings, Theme};
-use iced::widget::{Button, text};
-use counter::Counter;
+use iced::widget::{button, Button,Column, Text, ProgressBar, text};
+
 fn main() {
     GUIApp::run(Settings::default()).expect("Could not run application");
 }
+#[derive(Debug, Clone)]
+pub enum Message{
+    ButtonPressed,
+}
 
-struct GUIApp;
+struct GUIApp{
+    button: iced::widget::button::State,
+}
 
 impl Application for GUIApp {
     type Executor = executor::Default;
     type Flags = ();
-    type Message = ();
+    type Message = Message;
     type Theme = Theme;
     fn new(_flags: ()) -> (GUIApp, Command<Self::Message>) {
-        (GUIApp, Command::none())
+        (GUIApp{ button: iced::widget::button::State::new()}, Command::none())
     }
 
     fn title(&self) -> String {
         String::from("Install")
     }
 
-    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
+    fn update(&mut self, message: Message) -> Command<Self::Message> {
+        match message {
+            Message::ButtonPressed => {
+                println!("Testing action");
+            }
+        }
         Command::none()
     }
 
     fn view(&self) -> Element<Self::Message> {
-        button("Install").into()
+        let content = Column::new()
+            .spacing(20) // Spacing between elements
+            .push(Button::new(Text::new("Install")).on_press(Message::ButtonPressed))
+            .push(Button::new(Text::new("Cancel"))) 
+            .push(Text::new("App directory"))
+            .push(ProgressBar::new(0.0..=100.0, 50.0));
+
+        // Convert the layout into an Element to be displayed
+        content.into()
     }
 
     
-}
-fn button<'a, Message: Clone>(label: &str) -> Button<'a, Message> {
-    iced::widget::button(
-        text(label),
-    )
-    .padding(12)
-    .width(100)
 }
