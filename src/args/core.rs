9d1b27e97e56add1f38c6c2185e6201c8e6b0880
evermine18 <edgarsanou@gmail.com>
2024-01-30 20:29:18 +0100

    Function to check if an argument exists

--- src/args/core.rs
+++ src/args/core.rs
@@ -1,8 +1,9 @@
 
 use std::env;
 
-trait Arguments{
+pub trait Arguments{
     fn arguments(&self) -> String;
+    fn exists(&self) -> bool;
 }
 
 impl Arguments for String{
@@ -16,6 +17,12 @@ impl Arguments for String{
         }
         value
     }
+
+    fn exists(&self) -> bool{
+        // Gets the argument based on the argument name --example "value" and return the value
+        let args: Vec<String> = env::args().collect();
+        args.contains(&self.to_string())
+    }
 }
 
 impl Arguments for i32{
@@ -28,8 +35,18 @@ impl Arguments for i32{
         }
         value
     }
+
+    fn exists(&self) -> bool {
+        // Returns true if the argument exists
+        let args: Vec<String> = env::args().collect();
+        args.len() > *self as usize
+    }
 }
 
 pub fn get_argument<T: Arguments>(arg: T) -> String{
     arg.arguments()
+}
+
+pub fn argument_exists<T: Arguments>(arg: T) -> bool{
+    arg.exists()
 }
--- src/args/mod.rs
+++ src/args/mod.rs
@@ -3,5 +3,5 @@ mod core;
 pub fn is_install(){
     // Checks if the --install flag is present
     let installed_arg = String::from("--install");
-    println!("{}",core::get_argument(installed_arg));
+    println!("{}",core::argument_exists(installed_arg));
 }
